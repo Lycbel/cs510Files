a84a62bcca66b5360d4a836bd232416191d039f3
Andy <lycbel@gmail.com>
2018-09-27 12:46:15 -0700

    initiate:copy the origin curl, autoconfigure it locally  and modify MakeFile

---
+++ curl-origin/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ee3c5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_ee3c5.dir/HAVE_SGTTY_H.c.o"
+  "cmTC_ee3c5.pdb"
+  "cmTC_ee3c5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_ee3c5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ curl-origin/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ee3c5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_ee3c5.
+# This may be replaced when dependencies are built.
